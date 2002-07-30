c23ca3e2330166e834faf9c9207ff68b05460dfc
Marcus Schäfer <ms@suse.de>
2002-07-30 09:35:52 +0000

    - moved helper scripts from the tools to the scripts directory - add setVGA script used for the GrUB reconfiguration to the source - add GrUB update to x11main.ycp
    svn path=/trunk/x11/; revision=4051

---
+++ scripts/setVGA.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+# Copyright (c) 2002 SuSE GmbH Nuernberg, Germany.  All rights reserved.
+#
+# Author: Marcus Schaefer <sax@suse.de>, 2002
+# xapi script: add or modify vga value for GrUB kernel line
+# --
+#
+# CVS ID:
+# --------
+# Status: Up-to-date
+#
+use strict;
+
+#=================================
+# Globals...
+#---------------------------------
+my $kernel = $ARGV[0];
+my $vga    = $ARGV[1];
+
+#---[ update ]-----#
+sub update {
+#------------------------------------------------
+# check if the current kernel line contains
+# the vga value and update it with the new vga
+# value
+# 
+	if ($kernel =~ /.*vga=([0-9]+|normal|ext)/) {
+		$kernel =~ s/$1/$vga/;
+	} else {
+		$kernel = $kernel." vga=".$vga;
+	}
+	return (
+		$kernel
+	);
+}
+
+my $newLine = update();
+print "$newLine\n";
