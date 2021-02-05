7de7911f3dd8e851ec8445d6103c886707767ff4
Ladislav Slezák <lslezak@suse.cz>
2021-02-05 11:06:57 +0100

    Fixed comment

--- src/tools/active_window.c
+++ src/tools/active_window.c
@@ -11,7 +11,6 @@
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
  * more details.
- * Extended Window Manager Hints
  **/
 
 /**
