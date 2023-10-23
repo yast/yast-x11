c3fa4738efe1076bc23dee91d46ed4f2fa983349
Stefan Hundhammer <shundhammer@suse.de>
2023-10-23 13:41:18 +0200

    Formatting improvements
    Copyright header
    Comment wording
    Fewer decorative comments
    Braces and whitespace

--- src/tools/testX.c
+++ src/tools/testX.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2012 Novell, Inc.
+ * Copyright (c) 2023 SUSE Linux LLC
  *
  * All Rights Reserved.
  *
@@ -19,23 +20,6 @@
  * find current contact information at www.novell.com.
  */
 
-/**************
-FILE          : testX.c
-***************
-PROJECT       : SaX ( SuSE advanced X configuration )
-              :
-BELONGS TO    : Configuration tool X11 version 4.x
-              : YaST2 inst-sys tools
-              :
-DESCRIPTION   : Checks if the X server is ok and sets the root
-              : window's color. Forks a child that creates an
-              : invisible X client. The child exits when the
-              : X server exits.
-              :
-              : Exit code: 0: X server ok, 1: no X server.
-              :
-STATUS        : Status: Up-to-date
-**************/
 
 #include <stdio.h>
 #include <unistd.h>
@@ -47,9 +31,6 @@ STATUS        : Status: Up-to-date
 #include <sys/types.h>
 #include <sys/wait.h>
 
-//======================================
-// Defines
-//--------------------------------------
 #define ICEWM     "icewm"
 #define FVWM      "fvwm2"
 #define MWM       "mwm"
@@ -58,14 +39,8 @@ STATUS        : Status: Up-to-date
 #define ICEWMPREFS "preferences.yast2"
 #define FVWMRC     "fvwmrc.yast2"
 
-//======================================
-// Globals
-//--------------------------------------
 int screen;
 
-//======================================
-// Functions
-//--------------------------------------
 Cursor CreateCursorFromName(Display* dpy, const char* name);
 XColor NameToXColor(Display* dpy, const char* name, unsigned long pixel);
 int RunWindowManager(void);
@@ -79,16 +54,20 @@ int main(int argc, char** argv)
     char* cname;
     XColor color;
     Atom prop;
-    Pixmap save_pixmap = (Pixmap)None;
+    Pixmap save_pixmap = (Pixmap) None;
 
     //============================================
     // open display and check if we got a display
     //--------------------------------------------
     display = XOpenDisplay(NULL);
-    if (!display) {
+
+    if (!display)
+    {
 	exit (1);
     }
-    if ((argc == 2) && (strcmp(argv[1], "--fast") == 0)) {
+
+    if ((argc == 2) && (strcmp(argv[1], "--fast") == 0))
+    {
 	XCloseDisplay(display);
 	exit (0);
     }
@@ -101,8 +80,10 @@ int main(int argc, char** argv)
     root = RootWindow(display, screen);
     pixel = BlackPixel(display, screen);
 
-    if (XParseColor(display, DefaultColormap(display, screen), cname, &color)) {
-	if (XAllocColor(display, DefaultColormap(display, screen), &color)) {
+    if (XParseColor(display, DefaultColormap(display, screen), cname, &color))
+    {
+	if (XAllocColor(display, DefaultColormap(display, screen), &color))
+        {
 	    pixel = color.pixel;
 	}
     }
@@ -110,16 +91,17 @@ int main(int argc, char** argv)
     XClearWindow(display, root);
 
     //============================================
-    // set watch cursor
+    // set the cursor
     //--------------------------------------------
     cursor = CreateCursorFromName(display, "top_left_arrow");
-    if (cursor) {
+    if (cursor)
+    {
 	XDefineCursor(display, root, cursor);
 	XFreeCursor(display, cursor);
     }
 
     //============================================
-    // run the windowmanager (FVWM)
+    // start a window manager
     //--------------------------------------------
     RunWindowManager();
 
@@ -133,15 +115,13 @@ int main(int argc, char** argv)
     XSetCloseDownMode(display, RetainPermanent);
 
     //============================================
-    // close display and exit
+    // Shut down
     //--------------------------------------------
     XCloseDisplay(display);
-    exit (0);
+    exit(0);
 }
 
-//=========================================
-// CreateCursorFromName
-//-----------------------------------------
+
 Cursor CreateCursorFromName(Display* dpy, const char* name)
 {
     XColor fg, bg;
@@ -154,34 +134,35 @@ Cursor CreateCursorFromName(Display* dpy, const char* name)
     bg = NameToXColor(dpy, back_color, WhitePixel(dpy, screen));
 
     i = XmuCursorNameToIndex(name);
+
     if (i == -1)
 	return (Cursor) 0;
     fid = XLoadFont (dpy, "cursor");
     if (!fid)
 	return (Cursor) 0;
+
     return XCreateGlyphCursor(dpy, fid, fid, i, i+1, &fg, &bg);
 }
 
-//=========================================
-// NameToXColor
-//-----------------------------------------
+
 XColor NameToXColor(Display* dpy, const char* name, unsigned long pixel)
 {
     XColor c;
 
-    if (!name || !*name) {
+    if (!name || !*name)
+    {
 	c.pixel = pixel;
 	XQueryColor(dpy, DefaultColormap(dpy, screen), &c);
-    } else if (!XParseColor(dpy, DefaultColormap(dpy, screen), name, &c)) {
+    }
+    else if (!XParseColor(dpy, DefaultColormap(dpy, screen), name, &c))
+    {
 	fprintf(stderr, "testX: unknown color or bad color format: %s\n", name);
 	exit(1);
     }
+
     return c;
 }
 
-//=========================================
-// RunWindowManager
-//-----------------------------------------
 int RunWindowManager(void)
 {
     int wmpid = fork();
